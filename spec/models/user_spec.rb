ed048b96e4400d05e4f428a622346a25df8ed1bb
siriussky <swt.lmn@gmail.com>
2012-07-25 16:41:43 +0530

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,8 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-  it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }  
 
   it { should be_valid }
 
@@ -108,6 +109,10 @@ describe User do
       @user.reload.email.should == mixed_case_email.downcase
     end
   end
-
+ 
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,6 +19,11 @@ describe "Authentication" do
 
       it { should have_selector('title', text: 'Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
     end
   end
 
@@ -34,8 +39,11 @@ describe "Authentication" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-    end
+    
   end
 
-
+  describe "followed by signout" do
+      before { click_link "Sign out" }
+      it { should have_link('Sign in') }
+  end
 end
